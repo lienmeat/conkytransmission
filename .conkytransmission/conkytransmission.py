3582482a52fa2ed69002052ec93e46b29fd75576
Eric Lien <liener@lienbeast>
2010-08-26 17:28:01 -0700

    Added the "extra torrent data" feature, which calls transmission-remote -t <torrent_id> -i for each torrent to be shown and parses additional data not available to the regular transmission-remote -l command.  This is slower though!
    Have not added all possible data to the template engine yet, but a very good subset is there automatically.
    
    (Still need to document everything)

--- .conkytransmission/conkytransmission.py
+++ .conkytransmission/conkytransmission.py
@@ -27,6 +27,7 @@ import codecs
 from optparse import OptionParser
 from subprocess import Popen, PIPE
 from operator import attrgetter
+from datetime import datetime
 
 class CommandLineParser:
 
@@ -36,6 +37,7 @@ class CommandLineParser:
         self.parser = OptionParser()
         self.parser.add_option("-a","--showactive", dest="show_active", default=False, action="store_true", help=u"Show only active torrents")
         self.parser.add_option("--case_sensitive", dest="case_sensitive_filter", default=False, action="store_true", help=u"Make the keyword filter case sensitive")
+        self.parser.add_option("-e","--extradata", dest="extra_data", default=False, action="store_true", help=u"Get extra torrent data (runs a command for every torrent to get more data, slower)")
         self.parser.add_option("-f","--filterlist", dest="filter_file", default=False, metavar="FILE", help=u"File containing keywords to filter out torrents with. If the keyword is found in a torrent name, that torrent will not be shown.")
         self.parser.add_option("-k","--kbps",dest="k_unit", default="K", type="string", metavar="STRING", help=u"How you would like KiB/s to be shown [default: %default]")
         self.parser.add_option("-l","--namelength",dest="name_length", type="int", default=35, metavar="NUMBER", help=u"[default: %default] Length of torrent name in characters")
@@ -90,9 +92,9 @@ class ConkyTransmission:
     #checks for filter words in torrent name
     def hasFilterWord(self, torrent):
         if self.config.case_sensitive_filter:
-            name = torrent.file                    
+            name = torrent.name                    
         else:
-            name = torrent.file.lower()
+            name = torrent.name.lower()
 
         for w in self.filter_list:
             if name.find(w) >= 0:
@@ -102,10 +104,8 @@ class ConkyTransmission:
     # scrape data from transmission-remote, separating lines
     # returns list of lines of transmission-remote -l
     def scrapeTransmission(self):
-        p = Popen(["transmission-remote", "-l"], stdout=PIPE, stderr=PIPE)
-        output = p.communicate()[0]
-        return output.splitlines()
-
+        return getCommandOutput(["transmission-remote", "-l"])
+        
     # Gathers output for conky into variables
     def getTorrentData(self):
         havetorrents = False
@@ -118,9 +118,13 @@ class ConkyTransmission:
                 t = Torrent(t, self.config)
                 if self.config.show_active:
                     if(t.status == "Seeding" or t.status == "Downloading" or t.status == "Up & Down" and not self.hasFilterWord(t)):
+                        if self.config.extra_data:
+                            t.setExtraData(self.getExtraData(t))
                         self.torrent_list.append(t)
                         havetorrents = True
                 elif(not self.hasFilterWord(t)):
+                    if self.config.extra_data:
+                        t.setExtraData(self.getExtraData(t))
                     havetorrents = True
                     self.torrent_list.append(t)
             #last line has relevent global stats
@@ -129,7 +133,11 @@ class ConkyTransmission:
                 self.getGlobalStats(t)
             count = count + 1
         return havetorrents
-    
+        
+    def getExtraData(self, torrent):
+        if self.config.extra_data:
+            return getCommandOutput(["transmission-remote", "-t", str(torrent.id), "-i"])
+            
     #sort logic wrapper
     #calls multiple sort methods      
     def sortTorrents(self):
@@ -137,7 +145,7 @@ class ConkyTransmission:
         if sb == "eta":
             self.simpleSort("eta_seconds")
         elif sb == "name":
-            self.simpleSort("file")
+            self.simpleSort("name")
         elif sb == "progress":
             self.sortByProgress()
         else:
@@ -185,17 +193,33 @@ class ConkyTransmission:
             print self.templater.getOutput()
             
 class Torrent:
-    """Parses out torrent properties from a line of output of transmission-remote -l"""
-    id = 0
-    percent = 0
-    downloaded = ""
-    eta = ""
-    eta_seconds = 0
-    up = 0.0
-    down = 0.0
-    ratio = 0.00
-    status = ""
-    file = ""
+    """Parses out torrent properties from a lines of output of transmission-remote"""
+    id = None
+    percent = None
+    eta = None
+    eta_seconds = None
+    up = None
+    down = None
+    ratio = None
+    status = None
+    name = None
+    location = None
+    available = None
+    size = None
+    downloaded = None
+    uploaded = None
+    ratio_limit = None
+    corrupt = None
+    connected_to = None
+    uploading_to = None
+    downloading_from = None
+    datetime_added = None
+    datetime_started = None
+    datetime_latest_activity = None
+    public_torrent = None
+    pieces = None
+    piece_size = None
+    
 
     # parses an input line of torrent properties into
     # class variables
@@ -212,8 +236,6 @@ class Torrent:
                     self.setId(p)
                 elif count == 2:
                     self.setPercent(p)
-                elif count == 3:
-                    self.downloaded = p
                 elif count == 4:
                     self.setETA(p)
                 elif count == 5:
@@ -225,7 +247,7 @@ class Torrent:
                 elif count == 8:
                     self.status = p
                 elif count == 9:
-                    self.file = p
+                    self.name = p
         #for some crazy reason, sometimes transmission-remote returns
         #the wrong status
         if self.status == "Up & Down" and float(self.down) < 0.1:
@@ -236,7 +258,116 @@ class Torrent:
             self.status = "Idle"
         elif self.status == "Downloading" and float(self.down) < 0.1:
             self.status = "Idle"
+            
+    def setExtraData(self, data):
+        for l in data:
+            if self.setLocation(l): continue
+            if self.setAvailable(l): continue
+            if self.setSize(l): continue
+            if self.setDownloaded(l): continue
+            if self.setUploaded(l): continue
+            if self.setRatioLimit(l): continue
+            if self.setCorrupt(l): continue
+            if self.setPeers(l): continue
+            if self.setDateAdded(l): continue
+            if self.setDateStarted(l): continue
+            if self.setLatestActivity(l): continue
+            if self.setPublicTorrent(l): continue
+            if self.setPieces(l): continue
+            if self.setPieceSize(l): continue
+
+    def getValue(self, line, remove):
+        return line.replace(remove, "").strip()
+            
+    def setLocation(self, line):
+        if self.location == None:
+            if line.find("Location:") >= 0:
+                self.location = self.getValue(line, "Location: ")
+                return True       
     
+    def setAvailable(self, line):
+        if self.available == None:
+            if line.find("Availability:") >= 0:
+                self.available = self.getValue(line, "Availability: ")
+                return True
+                
+    def setSize(self, line):
+        if self.size == None:
+            if line.find("Total size:") >= 0:
+                idx1 = line.find("(")
+                idx2 = line.find(")")
+                self.size = line[idx1+1:idx2].replace(" wanted", '')
+                return True
+            
+    def setDownloaded(self, line):
+        if self.downloaded == None:
+            if line.find("Downloaded:") >= 0:
+                self.downloaded = self.getValue(line, "Downloaded: ")
+                return True
+
+    def setUploaded(self, line):
+        if self.uploaded == None:
+            if line.find("Uploaded:") >= 0:
+                self.uploaded = self.getValue(line, "Uploaded: ")
+                return True
+            
+    def setRatioLimit(self, line):
+        if self.ratio_limit == None:
+            if line.find("Ratio Limit:") >= 0:
+                self.ratio_limit = self.getValue(line, "Ratio Limit: ")
+                return True
+            
+    def setCorrupt(self, line):
+        if self.corrupt == None:
+            if line.find("Corrupt DL:") >= 0:
+                self.corrupt = self.getValue(line, "Corrupt DL: ")
+                return True
+            
+    def setPeers(self, line):
+        if self.connected_to == None:
+            if line.find("Peers:") >= 0:
+                peers = self.getValue(line, "Peers: ").split(", ")
+                self.connected_to = peers[0].replace("connected to ", '').strip()
+                self.uploading_to = peers[0].replace("uploading to ", '').strip()
+                self.downloading_from = peers[0].replace("downloading from ", '').strip()
+                return True
+                
+    def setDateAdded(self, line):
+        if self.datetime_added == None:
+            if line.find("Date added:") >= 0:
+                self.datetime_added = datetime.strptime(self.getValue(line, "Date added: "), "%a %b %d %H:%M:%S %Y")
+                return True
+
+    def setDateStarted(self, line):
+        if self.datetime_started == None:
+            if line.find("Date started:") >= 0:
+                self.datetime_started = datetime.strptime(self.getValue(line, "Date started: "), "%a %b %d %H:%M:%S %Y")
+                return True
+        
+    def setLatestActivity(self, line):
+        if self.datetime_latest_activity == None:
+            if line.find("Latest activity:") >= 0:
+                self.datetime_latest_activity = datetime.strptime(self.getValue(line, "Latest activity: "), "%a %b %d %H:%M:%S %Y")
+                return True
+    
+    def setPublicTorrent(self, line):
+        if self.public_torrent == None:
+            if line.find("Public torrent:") >= 0:
+                self.public_torrent = self.getValue(line, "Public torrent: ")
+                return True
+                
+    def setPieces(self, line):
+        if self.pieces == None:
+            if line.find("Piece Count:") >= 0:
+                self.pieces = self.getValue(line, "Piece Count: ")
+                return True
+    
+    def setPieceSize(self, line):
+        if self.piece_size == None:
+            if line.find("Piece Size:") >= 0:
+                self.piece_size = self.getValue(line, "Piece Size: ")
+                return True
+
     # sets id of torrent
     def setId(self, str):
         self.id = int(str.strip("*"))
@@ -319,17 +450,12 @@ class TemplateWriter:
     def getTorrentOutput(self, torrents):
         for torrent in torrents:
             template = self.getTorrentTemplate(torrent)
-            template = template.replace("[:PERCENT:]", `torrent.percent`)
-            template = template.replace("[:DOWNLOADED:]", torrent.downloaded)
-            template = template.replace("[:ETA:]", torrent.eta)
-            template = template.replace("[:ETA_SECONDS:]", `torrent.eta_seconds`)
-            template = template.replace("[:UP:]", self.getSpeed(torrent.up))
-            template = template.replace("[:UP_KBPS:]", torrent.up)
-            template = template.replace("[:DOWN:]", self.getSpeed(torrent.down))
-            template = template.replace("[:DOWN_KBPS:]", torrent.down)
-            template = template.replace("[:RATIO:]", torrent.ratio)
-            template = template.replace("[:STATUS:]", torrent.status) 
-            template = template.replace("[:NAME:]", torrent.file[:self.config.name_length])
+            torrent.name = torrent.name[:self.config.name_length]
+            for p, v in torrent.__dict__.iteritems():
+                template = template.replace("[:"+p.upper()+":]", str(v))
+            template = template.replace("[:UP_KBPS:]", str(getSpeed(torrent.up, self.config.m_unit, self.config.k_unit)))
+            template = template.replace("[:DOWN_KBPS:]", str(getSpeed(torrent.down, self.config.m_unit, self.config.k_unit)))
+            #add additional template things here!
             self.torrent_output+=template        
         
     def getTorrentTemplate(self, torrent):
@@ -378,6 +504,11 @@ def getFile(path):
         return False
     else:
         return filedata
+        
+def getCommandOutput(command_list):
+    p = Popen(command_list, stdout=PIPE, stderr=PIPE)
+    output = p.communicate()[0]
+    return output.splitlines()
 
 if __name__ == "__main__":
     (options, args) = CommandLineParser().parse_args()
